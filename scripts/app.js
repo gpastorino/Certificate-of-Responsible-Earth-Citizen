08263dc5fa15ec31ead6485bcfedfa3595705f92
gpastorino@gmail.com <gpastorino>
2020-01-03 16:15:02 -0800

    Planning Phase For Plastics Recycling Game

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+console.log('Welcome to Match to Category...');
+
+
+
+$('button').on('click', ()=>{
+    console.log('Game Start')
+});  //this codes the button
+
+
+
+// == User Stories/game logic == //
+
+//When a picture pops up, then select the category button that it belongs in.  
+
+
+
+
+// When the user clicks begin, the timer should start and the squares should populate with a random color
+
+// When the user clicks on a color: the color should disapear and score should be added or subtracted.
+
+// When the round is over, the scores round and timer should be updated for the user to start over with increased difficulty.
