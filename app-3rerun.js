c9b0316f885729d5836435e1380746362c763e27
gpastorino@gmail.com <gpastorino>
2020-01-09 16:49:52 -0800

    debugging an issue with score. Also, my images array isn't reading, so i've redirected the images to default to just 4 images

---
+++ app-3rerun.js
@@ -0,0 +1,150 @@
+
+// const handlePoke = (event) => {
+//   // console.log('first check point');
+//   if(event.target.classList.contains('poked') === false){
+//     const color = $(event.target).css('background-color');
+//     $(event.target).addClass('poked').css('opacity', 0.3);
+//     checkValidPoke(color);
+
+  // }
+//this is where handlePoke is called:
+  // $('.squares').on('click','.square', handlePoke);
+
+
+// const checkValidPoke = color => {
+//   console.log({color});
+//   const colorValues = color.substring(4, color.length -1).split(', ');
+//   console.log(colorValues[2]);
+  
+//   if(colorValues[2] === "255"){
+  
+//       score++
+//       $('h1').text(`Scoreboard: ${score}`);
+  
+//   }else{
+//       score--
+//       $('h1').text(`Scoreboard: ${score}`);
+//   }  
+
+  // document.getElementById("myDiv").style.marginLeft = rightMarginPercent[index];
+  check();
+
+    // `${a}px ${b}px ${c}px ${d}px`
+// }
+
+//early research
+//  const itemsArray = ['compost-item','grocerystore-item', 'askcity-item' ,'local-recycle-item'];
+//       const index = Math.floor(Math.random() * itemsArray.length);
+//       return itemsArray[index];
+//     }
+
+
+    // document.querySelector(".grocery").addEventListener("click", ()=>{
+    //   score++
+    //   updateScore();
+    //   let d = document.getElementsByClassName("grocerystore-item");
+    //   if(d){
+    //     d[0].remove();
+    //     addItem();
+    //     console.log('grocery to be item removed!');
+    //   }
+    //   console.log(`this is the  ${score}`);
+    // });
+
+        // randomItem
+    //location for random location function:
+// ================================================================================//
+
+// const color = $(event.target).css('background-color');
+// document.getElementById("myDiv").style.marginLeft = "50px";
+//then, can I do this?
+// document.getElementById("myDiv").style.marginLeft = rightMarginPercent[index];
+
+//=================================================================================//
+
+
+
+
+// function randomLocation(){
+// const valueArray = ['a', 'b', 'c', 'd'];
+//repeat the math.floor(Math.random) 4 times. ONe for each of the values, then check if they produce random locations for one of the attributes.  
+//constraints for these to work in.  
+// width: 800px;
+// height: 100px;
+
+
+
+//creating array of pictures for each category and randomizing each picture.  
+    // function randomizePix() {
+    //   var randomNum = Math.floor(Math.random() * myPix.length);
+    //   document.getElementById("myPicture").src = myPix[randomNum];
+    // }
+
+
+      // randomizePix(askCityPix);
+
+
+      //need the button to the replay connected to this game over div
+//need this div to appear in front of everything at the main page.  
+
+
+
+  // document.getElementById("myBtn").addEventListener("click", displayDate);
+  // blotted out to get the lose working.
+  // var play= document.createElement('div');
+  // play.id="round3";
+  // document.getElementById("game-over").appendChild(play);
+  // document.getElementById('game-over').appendChild(play);
+
+
+
+
+
+// ==================================================================================//
+
+// var localRecycle = document.createElement('div');
+// localRecycle.classList.add('recycle');
+// // var localRecycleImage = document.createElement('img');
+// // localRecycleImage.setAttribute('src','imgs/battery1.png');
+// var aRealItem = document.getElementsByClassName('local-recycle-item');
+// aRealItem.appendChild(localRecycle);
+// aRealItem.appendChild(localRecycleImage);
+
+      // var localRecycle = document.createElement('div');
+      // var localRecycleImage = document.createElement('img');
+      // localRecycleImage.setAttribute('src','imgs/battery1.png');
+      // var aRealItem = document.getElementsByClassName('local-recycle-item');
+      // aRealItem.appendChild(localRecycle);
+      // aRealItem.appendChild(localRecycleImage);
+      
+
+
+        // document.getElementById('items').remove();
+
+
+  //this is where I could add the remove other item after the try again button is started.
+
+  // if(document.getElementsByClassName('local-recycle-item')===true){
+
+  //   document.getElementsByClassName('local-recycle-item').remove();
+
+  // }else if(document.getElementsByClassName('compost-item')===true){
+
+  //   document.getElementsByClassName('compost-item').remove();
+
+  // }else if(document.getElementsByClassName('askcity-item')===true){
+
+  //   document.getElementsByClassName('askcity-item').remove();
+
+  // }else{
+
+  //   document.getElementsByClassName('grocerystore-item').remove();
+  // }
+  
+  // document.body.remove();
+
+  // $('div#items').remove();
+
+
+
+  
--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,21 @@
 
-//revisit the positioning issue with the DOM for the try again page.  
+//make more than 4 images populate
 
-//randomize the block locations
+//fix flexbox
 
-//populate the Certificate
+//create designs
+
+
+//where do i put this?  
+//$('#items').empty();
+
+//when i hit the replay, it starts to score twice.  
 
 let time;
 let score;
 console.log('Welcome to Earthling Resposibility Program...');
 
 document.querySelector(".round2").addEventListener("click", playForCertificate);
-
-
-// function randomLocation(){
-// const valueArray = ['a', 'b', 'c', 'd'];
-//repeat the math.floor(Math.random) 4 times. ONe for each of the values, then check if they produce random locations for one of the attributes.  
-//constraints for these to work in.  
-// width: 800px;
-// height: 100px;
-
 
 
 function randomizeItemLocation(){
@@ -42,7 +39,7 @@ function playForCertificate(){
       updateScore();
     
       let d = document.getElementsByClassName("grocerystore-item");
-      if(d){
+      if(d[0]){
         d[0].remove();
         addItem();
         console.log('grocery to be item removed!');
@@ -57,7 +54,8 @@ function playForCertificate(){
       // document.getElementsByClassName("compost-item").style.margin = "`${a}px ${b}px ${c}px ${d}px`";
 
       let c = document.getElementsByClassName("compost-item");
-      if(c){
+      if(c[0]){
+        console.log(c);
         c[0].remove();
         console.log('compost paired');
         addItem();
@@ -69,7 +67,7 @@ function playForCertificate(){
       score++
       updateScore();
       let e = document.getElementsByClassName("askcity-item");
-      if(e){
+      if(e[0]){
 
         e[0].remove();
         addItem();
@@ -84,7 +82,7 @@ function playForCertificate(){
       let f = document.getElementsByClassName("local-recycle-item");
 
 
-      if(f){
+      if(f[0]){
         f[0].remove();
         addItem();
         console.log('local recycle block removed');
@@ -92,7 +90,7 @@ function playForCertificate(){
     });
 
     console.log(`this is the  ${score}`);
-  }
+}
 
 
 function addItem(){
@@ -106,7 +104,51 @@ function addItem(){
     randomItem.className = generateItemClassName();
 
 
+    if(randomItem === 'grocerystore-item'){
+    // if(randomItem == itemsArray[1]){
+      // 
+      function randomizePix(groceryPix) {
+        var randomNum = Math.floor(Math.random() * arr.length);
+        document.getElementsByClassName("grocerystore-item").src = arr[randomNum];
+      }
 
+      let groceryPix = ['../imgs/plasticbag1a.png','../imgs/plasticbag2 copy-add1.png'];
+      randomItem.style.backgroundImage = randomizePix(groceryPix);
+
+    }else if( randomItem =='compost-item'){
+    // }else if( randomItem ==itemsArray[0]){
+      // 
+      function randomizePix(compostPix) {
+        var randomNum = Math.floor(Math.random() * compostPix.length);
+        document.getElementsByClassName("compost-item").src = compostPix[randomNum];
+      }
+      let compostPix = ['../imgs/pizzaboxright.png', '../imgs/CompostA-add1.png', '../imgs/compostB-add 2.png'];
+      randomItem.style.backgroundImage = randomizePix(compostPix);
+
+      // randomizePix(compostPix);
+
+
+    }else if(randomItem === 'askcity-item'){
+    // }else if(randomItem == itemsArray[2]){
+      // 
+      function randomizePix(askCityPix) {
+        var randomNum = Math.floor(Math.random() * askCityPix.length);
+        document.getElementsByClassName("askcity-item").src = askCityPix[randomNum];
+      }
+      let askCityPix = ['../imgs/shreddedperfect.png', '../imgs/shredded-add2.png','../imgs/shredded-add3.png'];
+      randomItem.style.backgroundImage = randomizePix(askCityPix);
+
+    }else{
+
+      function randomizePix(localRecyclePix) {
+        var randomNum = Math.floor(Math.random() * localRecyclePix.length);
+        document.getElementsByClassName("local-recycle-item").src = localRecyclePix[randomNum];
+      }
+      let localRecyclePix =['../imgs/battery-add1.png','../imgs/battery-add2.png', '../imgs/battery-add3.png'];
+      randomItem.style.backgroundImage = randomizePix(localRecyclePix);
+
+    }
+  
     function check(){
 
       const leftMarginPercent =['20%', '25%', '30%','35%', '40%', '45%', '50%','55%', '60%','65%', '70%'];
@@ -114,21 +156,8 @@ function addItem(){
       return leftMarginPercent[index];
     }
 
-
     //remember to only reference the dom when something is in the dom.  Not before.  This isn't in the dom yet, and can just work as a free variable assigned a new attribite. 
-
     randomItem.style.margin = `90 0 0 ${check()}`;
-
-    // randomItem
-    //location for random location function:
-// ================================================================================//
-
-// const color = $(event.target).css('background-color');
-// document.getElementById("myDiv").style.marginLeft = "50px";
-//then, can I do this?
-// document.getElementById("myDiv").style.marginLeft = rightMarginPercent[index];
-
-//=================================================================================//
 
     document.querySelector(".items").appendChild(randomItem);
 }
@@ -150,9 +179,20 @@ const setTimer = () => {
         if(score>=10){
           certificate();
 
+          scoreReportWin();
+
+          $('#items').empty();
         }else{
           gameOverLose();
+
+          scoreReportLose();
+
           gameEnd(timer);
+
+
+  //this should occur when time runs out and the prompt to retry shows up.  
+
+  $('#items').empty();
 
         }
       }
@@ -177,79 +217,114 @@ function gameEnd(timer){
 
     clearInterval(timer);
 
+
+
+
 }
 function certificate(){
 
-  document.querySelector('body1').remove();
-  document.querySelector('footer').remove();
+  gameOverWin();
 
-  var certificateOnDOM = document.createElement('div');
-  certificateOnDOM.id="cert";
-  certificateOnDOM.innerText= "Here's Your Excellent Certificate of Excellence!";
-  document.body.appendChild(certificateOnDOM);
+  // document.querySelector('body1').remove();
+  // document.querySelector('footer').remove();
+
+  // var certificateOnDOM = document.createElement('div');
+  // certificateOnDOM.id="cert";
+  // certificateOnDOM.innerText= "Here's Your Excellent Certificate of Excellence!";
+  // document.body.appendChild(certificateOnDOM);
+
+
 }
 
 function gameOverWin(){
 
-  document.querySelector('body1').remove();
-  document.querySelector('footer').remove();
+  // document.querySelector('body1').remove();
+  // document.querySelector('footer').remove();
 
-}
+    console.log('game over - but,  SUCCESS')
+    var certificateOnDOM = document.createElement('div');
+    certificateOnDOM.id="certificate";
+    // gameOverOnDOM.style.position="fixed";
+    certificateOnDOM.innerText= "CONGRATULATIONS!  Print this window for for your certification.";
+    certificateOnDOM.innerHTML+= "<div id=try-again2></div>"
+    document.body.appendChild(certificateOnDOM);
+  
+    console.log(document.getElementById('try-again2'));
+  
+    document.getElementById('try-again2');
+  
+    document.getElementById('try-again2').addEventListener("click", reRun);
+  
+  }
 
 
-//need the button to the replay connected to this game over div
-//need this div to appear in front of everything at the main page.  
 
 function reRun(){
 
+  document.getElementById('certificate').remove();
+  resetScore();
+  playForCertificate();
+
+}
+
+function reRun2(){
+
   document.getElementById('game-over').remove();
-
-
+  resetScore();
   playForCertificate();
 
 }
 
 
+
 function gameOverLose(){
+
   console.log('game over lose trigger')
   var gameOverOnDOM = document.createElement('div');
+
   gameOverOnDOM.id="game-over";
-  // gameOverOnDOM.style.position="fixed";
+
   gameOverOnDOM.innerText= "Try Again!";
-  gameOverOnDOM.innerHTML+= "<div id=try-again></div>"
+  gameOverOnDOM.innerHTML+= "<div id='try-again'></div>"
   document.body.appendChild(gameOverOnDOM);
 
   console.log(document.getElementById('try-again'));
 
   document.getElementById('try-again');
 
-  document.getElementById('try-again').addEventListener("click", reRun);
-  // document.getElementById("myBtn").addEventListener("click", displayDate);
-  // blotted out to get the lose working.
-  // var play= document.createElement('div');
-  // play.id="round3";
-  // document.getElementById("game-over").appendChild(play);
-  // document.getElementById('game-over').appendChild(play);
+  document.getElementById('try-again').addEventListener("click", reRun2);
 
 }
 
 
+function scoreReportLose(){
+
+  var scoreReporting = document.createElement('div');
+
+  scoreReporting.id="score-end";
+
+  scoreReporting.innerHTML+= `<div id='score-end'>Your FINAL SCORE is:  ${score} </div>`;
+  document.getElementById('game-over');
+  document.getElementById('game-over').appendChild(scoreReporting);
 
 
-// ==================================================================================//
+}
 
-// var localRecycle = document.createElement('div');
-// localRecycle.classList.add('recycle');
-// // var localRecycleImage = document.createElement('img');
-// // localRecycleImage.setAttribute('src','imgs/battery1.png');
-// var aRealItem = document.getElementsByClassName('local-recycle-item');
-// aRealItem.appendChild(localRecycle);
-// aRealItem.appendChild(localRecycleImage);
 
-      // var localRecycle = document.createElement('div');
-      // var localRecycleImage = document.createElement('img');
-      // localRecycleImage.setAttribute('src','imgs/battery1.png');
-      // var aRealItem = document.getElementsByClassName('local-recycle-item');
-      // aRealItem.appendChild(localRecycle);
-      // aRealItem.appendChild(localRecycleImage);
-      
+function scoreReportWin(){
+  var scoreReporting = document.createElement('div');
+
+  scoreReporting.id="score-end";
+
+  scoreReporting.innerHTML+= `<div id='score-end'>Your FINAL SCORE is:  ${score} </div>`;
+
+  document.getElementById('try-again2');
+
+  document.getElementById('certificate').appendChild(scoreReporting);
+
+}
+
+function resetScore(){
+let score=0;
+
+}
